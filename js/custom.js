55ae3c7ad1dd8e44c949dbb25c65d84ee1856cb5
MikeRedl314 <106673364+MikeRedl314@users.noreply.github.com>
2022-06-01 17:07:37 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+
+
+$(document).ready(function() {
+    
+    
+   
+   
+
+     $('#KontaktClick').click(function(){
+        document.getElementById('Kontakt').scrollIntoView({block: 'center'});
+          }); 
+    $('#HomeClick').click(function(){
+         document.getElementById('Homeview').scrollIntoView({block: 'center'});
+         }); 
+       
+
+    });
